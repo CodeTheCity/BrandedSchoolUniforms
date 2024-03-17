e6412b041dab81ccc196aabbaf257d3a87b0cf65
Luca1272 <119085376+Luca1272@users.noreply.github.com>
2024-03-17 11:33:36 +0000

    Navbar fixes

--- school-uniform-comparison/components/Nav.jsx
+++ school-uniform-comparison/components/Nav.jsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Nav = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -21,7 +21,7 @@ const Nav = () => {
       </Link>
       <section className="MOBILE-MENU flex lg:hidden">
         <div
-          className="HAMBURGER-ICON space-y-2"
+          className="HAMBURGER-ICON space-y-2 cursor-pointer"
           onClick={() => setIsNavOpen((prev) => !prev)}
         >
           <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
@@ -39,7 +39,6 @@ const Nav = () => {
           </ul>
         </div>
       </section>
-
       <ul className="DESKTOP-MENU hidden space-x-8 lg:flex flex-row items-center">
         <Link href='/' className='nav_link'>Home</Link>
         <Link href='/contact' className='nav_link'>Schools</Link>
@@ -47,24 +46,6 @@ const Nav = () => {
         <Link href='/about' className='nav_link'>About</Link>
         <Link href='/contact' className='black_btn'>Contact Us</Link>
       </ul>
-      <style jsx>{`
-        .hideMenuNav {
-          display: none;
-        }
-        .showMenuNav {
-          display: flex;
-          flex-direction: column;
-          position: absolute;
-          top: 100%; /* Dropdown starts right below the navbar */
-          left: 0; /* Aligns to the left edge of the viewport */
-          width: 100%; /* Ensures it spans the full width of the screen */
-          background: white;
-          border: 1px solid #e5e7eb; /* A slight border */
-          border-radius: 8px; /* Rounded corners */
-          overflow: hidden; /* Ensures content fits within the borders */
-          box-shadow: 0 2px 4px rgba(0,0,0,0.1); /* Subtle shadow for depth */
-        }
-      `}</style>
     </nav>
   );
 };
