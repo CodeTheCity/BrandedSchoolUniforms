117b9c486f82b77d16d18f5cfd2b85170556f961
hariesramdhani <hydrolizedmaltose@gmail.com>
2024-03-17 13:28:32 +0000

    Updated ASDA data

--- school-uniform-comparison/components/Feed.jsx
+++ school-uniform-comparison/components/Feed.jsx
@@ -1,81 +1,43 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
-
-import PromptCard from "./PromptCard";
-
-const PromptCardList = ({ data, handleTagClick }) => {
-  return (
-    <div className='mt-16 prompt_layout'>
-      {data.map((post) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
-      ))}
-    </div>
-  );
-};
 
 const Feed = () => {
   const [allPosts, setAllPosts] = useState([]);
-
-  // Search states
   const [searchText, setSearchText] = useState("");
-  const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("https://raw.githubusercontent.com/CodeTheCity/BrandedSchoolUniforms/scraper/data/sample.json");
-    const data = await response.json();
-
-    setAllPosts(data);
+    try {
+      const response = await fetch("https://raw.githubusercontent.com/CodeTheCity/BrandedSchoolUniforms/scraper/data/sample.json");
+      const data = await response.json();
+      setAllPosts(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
   };
 
   useEffect(() => {
     fetchPosts();
-  }, [allPosts]);
+  }, []);
+
+  useEffect(() => {
+    setSearchedResults(filterPrompts(searchText));
+  }, [searchText, allPosts]);
 
   const filterPrompts = (searchtext) => {
-    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
-    
-    console.log(allPosts.filter(
-      (item) =>
-        regex.test(item.school)
-    ))
-
-    setAllPosts(allPosts.filter(
-      (item) =>
-        regex.test(item.school)
-    ))
-    
-    return allPosts.filter(
-      (item) =>
-        regex.test(item.school)
-    );
-
+    const regex = new RegExp(searchtext, "i");
+    return allPosts.filter((item) => regex.test(item.school));
   };
 
   const handleSearchChange = (e) => {
-    clearTimeout(searchTimeout);
-    setSearchText(e.target.value);
-
-    // debounce method
-    setSearchTimeout(
-      setTimeout(() => {
-        const searchResult = filterPrompts(e.target.value);
-        setSearchedResults(searchResult);
-      }, 500)
-    );
+    const searchText = e.target.value;
+    setSearchText(searchText);
   };
 
   const handleTagClick = (tagName) => {
     setSearchText(tagName);
-
-    const searchResult = filterPrompts(tagName);
-    setSearchedResults(searchResult);
+    setSearchedResults(filterPrompts(tagName));
   };
 
   return (
@@ -92,23 +54,13 @@ const Feed = () => {
       </form>
 
       <div className="w-full mt-10 flex flex-row gap-x-5">
-        {allPosts.map((post, index) => (
+        {searchedResults.map((post, index) => (
           <div key={index} className="bg-amber-100 rounded-lg flex flex-col items-center p-4" style={{backgroundColor: 'rgba(255,223,155,0.5)'}}>
-            <img src={post.logo}/>
+            <img src={post.logo} alt={`Logo of ${post.school}`} />
             <p>{post.school}</p>
           </div>
         ))}
       </div>
-
-      {/* All Prompts */}
-      {/* {searchText ? (
-        <PromptCardList
-          data={searchedResults}
-          handleTagClick={handleTagClick}
-        />
-      ) : (
-        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
-      )} */}
     </section>
   );
 };
--- school-uniform-comparison/next.config.mjs
+++ school-uniform-comparison/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['www.border-embroideries.co.uk'],
     },
+    output: 'export',
 };
 
 export default nextConfig;
